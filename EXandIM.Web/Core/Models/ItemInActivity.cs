12213b373885f4fbbbad437d03702b7863e01092
Mohamed Abdelaziz <114187241+Mohamed-192002@users.noreply.github.com>
2025-03-12 00:50:13 +0200

    Add project files.

---
+++ EXandIM.Web/Core/Models/ItemInActivity.cs
@@ -0,0 +1,18 @@
+﻿namespace EXandIM.Web.Core.Models
+{
+    public class ItemInActivity
+    {
+        public int Id { get; set; }
+        public int? BookId { get; set; }
+        public Book? Book { get; set; }
+        public int? ReadingId { get; set; }
+        public Reading? Reading{ get; set; }
+        public string? Procedure { get; set; }
+        public DateTime? ProcedureDate { get; set; }
+        public int? ActivityBookId { get; set; }
+        public ActivityBook? ActivityBook { get; set; }
+
+        // New SortOrder field to define order within the ActivityBook
+        public int SortOrder { get; set; }
+    }
+}
