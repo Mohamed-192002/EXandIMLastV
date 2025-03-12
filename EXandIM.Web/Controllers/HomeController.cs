473d611fb0928416bbf79c4b7f021140c36825c5
Mohamed Abdelaziz <114187241+Mohamed-192002@users.noreply.github.com>
2025-03-12 02:23:48 +0200

    add dashboard

--- EXandIM.Web/Controllers/HomeController.cs
+++ EXandIM.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using EXandIM.Web.Core.Models;
+using EXandIM.Web.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,17 +10,37 @@ namespace EXandIM.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
+        public IActionResult GetCounts()
+        {
+            var counts = new
+            {
+                importNum = _context.Books.Where(x => !x.IsExport).Count(),
+                importNumPassed = _context.Books.Where(x => !x.IsExport && x.Passed).Count(),
+                importNumUnPassed = _context.Books.Where(x => !x.IsExport && !x.Passed).Count(),
+                exportNum = _context.Books.Where(x => x.IsExport).Where(x => x.IsExport).Count(),
+                exportNumPassed = _context.Books.Where(x => x.IsExport && x.Passed).Count(),
+                exportNumUnPassed = _context.Books.Where(x => x.IsExport && !x.Passed).Count(),
+                readingsNum = _context.Readings.Count(),
+                readingsNumPassed = _context.Readings.Count(r => r.Passed),
+                readingsNumUnPassed = _context.Readings.Count(r => !r.Passed),
+                activityNum = _context.Activities.Count()
+            };
+
+            return Json(counts);
+        }
         public IActionResult Index()
         {
             return View();
         }
-        [Authorize(Roles ="a")]
+        [Authorize(Roles = "a")]
         public IActionResult Privacy()
         {
             return View();
